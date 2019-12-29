24dbb73db3e95762c7b95d1f7eb8fde7ece32463
ash <ash@strategicdata.com.au>
2019-12-29 15:53:17 +1100

    Add config var for sprite flags/numbers

--- main.lua
+++ main.lua
@@ -1,11 +1,14 @@
-
+config = {
+    player_sprite = 5,
+    solid_flag = 0,
+}
 
 function _init()
     player = {}
     jump_timer = 4
 
     for y=0,63 do for x=0,127 do
-        if (mget(x,y) == 1) then
+        if (mget(x,y) == config.player_sprite) then
             player = make_player(x, y)
             mset(x, y, 0)
         end
@@ -14,7 +17,6 @@ end
 
 function _update()
     jump_timer -= 1 / 30
-
     update_player(player)
 end
 
@@ -22,6 +24,9 @@ function _draw()
     cls()
     map(0, 0, 0, 0)
 
-    print(flr(jump_timer), 0, 0)
+    -- wip camera
+    camera(player.x * 8 - 5 * 8, 0)
+
+    print(flr(jump_timer), 15 * 8, 1)
     draw_player(player)
 end
